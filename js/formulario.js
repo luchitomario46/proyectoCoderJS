64c560911b6f809dcd250f41ed1512f24657d136
luchitomario46 <83929510+luchitomario46@users.noreply.github.com>
2021-10-03 19:15:31 -0300

    Add files via upload
    Segundo commit, con ajustes en Formularios para agregar Jquery en este.

--- js/formulario.js
+++ js/formulario.js
@@ -1,15 +1,14 @@
 let miFormulario = document.getElementById("formulario");
 
-miFormulario.addEventListener("submit", validarFormulario);
+//miFormulario.addEventListener("submit", validarFormulario);
 
-function validarFormulario(e){
-    e.preventDefault();
-    let formulario = e.target
+$("div.botonSubmitClean").prepend('<button id="btnSend">Enviar</button>')
 
-    console.log(formulario.children[1].value);
-    console.log(formulario.children[3].value);
-    console.log(formulario.children[5].value);
-    console.log(formulario.children[7].value);
-    console.log(formulario.children[9].value);
-    console.log(formulario.children[11].value);
-}
+$("#btnSend").click((e) => {
+    alert("Nombre: " + formulario.children[1].value + " " + formulario.children[3].value);
+    alert("Pais agregado: " + formulario.children[7].value)
+    alert("Capital: " + formulario.children[9].value)
+    alert("Moneda: " + formulario.children[11].value)
+    alert("Continente: " + formulario.children[9].value)
+    alert("La informacion sera revisada por nuestro equipo, y posteriormente agregada a nuestra Base de Datos, muchas gracias por participas en Viajeros.com")
+});
